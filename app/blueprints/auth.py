e997180df020d000e96e7dc1c9891f69507cc9ae
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2026-02-07 19:56:09 -0300

    pronto

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
+# O segredo: Importamos o User aqui para as rotas usarem
 from app.models import User
 
 auth_bp = Blueprint('auth', __name__)
@@ -10,10 +11,13 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         
+        # Simulação sênior de login (admin/123)
         if username == 'admin' and password == '123':
-            user = User("1", "admin")
-            login_user(user) # <--- O Flask agora lembra de você!
+            user = User.get("1")
+            login_user(user)
             return redirect(url_for('web.index'))
+        else:
+            flash('Login inválido')
             
     return render_template('auth/login.html')
 
